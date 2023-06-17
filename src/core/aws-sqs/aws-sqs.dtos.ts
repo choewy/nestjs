3d4baa3d816a73cc616eeafeafaed5d87df2b33d
choewy <choewy32@gmail.com>
2023-06-17 19:15:03 +0900

    feature/sqs dtos and errors

---
+++ src/core/aws-sqs/aws-sqs.dtos.ts
@@ -0,0 +1,30 @@
+import { plainToInstance } from 'class-transformer';
+
+import { InvalidAwsSQSMessageBodyError } from './aws-sqs.error';
+import { AwsConfigFactory } from '../config';
+
+export class AwsSQSCredentials {
+  public static of(awsConfigFactory: AwsConfigFactory): AwsSQSCredentials {
+    return new AwsSQSCredentials(awsConfigFactory.accessKeyId, awsConfigFactory.secretAccessKey);
+  }
+
+  constructor(readonly accessKeyId: string, readonly secretAccessKey: string) {}
+}
+
+export class AwsSQSMessageBody<D = object> {
+  public static to<D = object>(subject: string, data: D): AwsSQSMessageBody<D> {
+    return new AwsSQSMessageBody(subject, data);
+  }
+
+  public static from<D = object>(messageBody: string): AwsSQSMessageBody<D> {
+    const body = plainToInstance(AwsSQSMessageBody, JSON.parse(messageBody));
+
+    if (!(body instanceof AwsSQSMessageBody)) {
+      throw new InvalidAwsSQSMessageBodyError();
+    }
+
+    return new AwsSQSMessageBody(body.subject, body.data);
+  }
+
+  constructor(readonly subject: string, readonly data: D) {}
+}
---
+++ src/core/aws-sqs/aws-sqs.error.ts
@@ -0,0 +1,1 @@
+export class InvalidAwsSQSMessageBodyError extends Error {}
