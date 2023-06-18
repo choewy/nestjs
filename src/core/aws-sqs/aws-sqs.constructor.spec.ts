7b5c8d32cfd6338c8a01750d12a1677c296809e9
choewy <choewy32@gmail.com>
2023-06-18 13:30:24 +0900

    refactor/aws-sqs.constructor test

--- src/core/aws-sqs/aws-sqs.constructor.spec.ts
+++ src/core/aws-sqs/aws-sqs.constructor.spec.ts
@@ -12,18 +12,18 @@ class AwsSQSTestConstructor extends AwsSQSConstructor {
 
 describe('AwsSQSConstructor', () => {
   const awsSQSConstructor = new AwsSQSTestConstructor(
-    AwsSQSTestConstructor.name,
+    'jest',
     'region',
     { accessKeyId: 'access', secretAccessKey: 'secret' },
-    'http://localhost:45661',
+    'https://jestjs.io',
     'test-queue',
   );
 
-  it('contextName을 조회하면 AwsSQSTestConstructor:test-queue가 반환되어야 한다.', () => {
-    expect(awsSQSConstructor.testContextName()).toEqual('AwsSQSTestConstructor:test-queue');
+  it('contextName을 조회하면 jest:test-queue가 반환되어야 한다.', () => {
+    expect(awsSQSConstructor.testContextName()).toEqual('jest:test-queue');
   });
 
-  it('queueUrl을 조회하면 http://localhost:45661/test-queue가 출력되어야 한다.', () => {
-    expect(awsSQSConstructor.testQueueUrl()).toEqual('http://localhost:45661/test-queue');
+  it('queueUrl을 조회하면 https://jestjs.io/test-queue가 출력되어야 한다.', () => {
+    expect(awsSQSConstructor.testQueueUrl()).toEqual('https://jestjs.io/test-queue');
   });
 });
