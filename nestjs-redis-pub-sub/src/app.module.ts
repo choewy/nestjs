23b7e93ce8d681ca7d52740996050d70462b26bc
choewy <choewy32@gmail.com>
2023-11-04 01:38:17 +0900

    feature/app module

--- nestjs-redis-pub-sub/src/app.module.ts
+++ nestjs-redis-pub-sub/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+
+import { RedisModule } from '@core/redis';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { CheckoutModule } from '@module/checkout';
+import { HostModule } from '@module/host';
+import { GuestModule } from '@module/guest';
+
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), EventEmitterModule.forRoot(), RedisModule, CheckoutModule, HostModule, GuestModule],
   controllers: [AppController],
   providers: [AppService],
 })
