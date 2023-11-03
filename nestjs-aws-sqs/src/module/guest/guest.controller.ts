b84162ee1a9b9bb649423bf41092b382d2a2f618
choewy <choewy32@gmail.com>
2023-11-03 20:01:50 +0900

    feature/guest module

---
+++ nestjs-aws-sqs/src/module/guest/guest.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { OnEvent } from '@nestjs/event-emitter';
+
+import { GuestMessageEvent } from '@common/events';
+
+import { GuestService } from './guest.service';
+
+@Controller('guest')
+export class GuestController {
+  constructor(private readonly guestService: GuestService) {}
+
+  @Get('send/:message')
+  async sendMessageToHost(@Param('message') message: string) {
+    return this.guestService.sendMessageToHost(message);
+  }
+
+  @OnEvent(GuestMessageEvent.Subject, { suppressErrors: false })
+  async onMessage(event: GuestMessageEvent) {
+    console.log(event);
+  }
+}
