18abca0daa8f202b07a5da5fb1bdf6487c349465
choewy <choewy32@gmail.com>
2023-11-03 20:02:08 +0900

    feature/app module

--- nestjs-aws-sqs/src/app.module.ts
+++ nestjs-aws-sqs/src/app.module.ts
@@ -1,16 +1,26 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { MongoConfig } from '@common/configs';
+import { MongoDBConnectionName, MongoDBName } from '@common/enums';
+import { AwsSQSModule } from '@core/aws-sqs';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { MongoConfig } from '@common/configs';
-import { MongoDBConnectionName, MongoDBName } from '@common/enums';
+
+import { HostModule } from '@module/host';
+import { GuestModule } from '@module/guest';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    EventEmitterModule.forRoot(),
     MongooseModule.forRoot(...new MongoConfig().getModuleOptions(MongoDBName.Logs, MongoDBConnectionName.Logger)),
+    AwsSQSModule,
+    HostModule,
+    GuestModule,
   ],
   controllers: [AppController],
   providers: [AppService],
