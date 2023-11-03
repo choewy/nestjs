46e2a28d6e70880e74ba26a933a583dac0d7479f
choewy <choewy32@gmail.com>
2023-11-04 00:31:56 +0900

    feature/redis config

---
+++ nestjs-redis-pub-sub/src/common/configs/redis.config.ts
@@ -0,0 +1,22 @@
+import { ConfigService } from '@nestjs/config';
+import { RedisOptions } from 'ioredis';
+
+export class RedisConfig {
+  private readonly configService = new ConfigService();
+
+  private readonly REDIS_HOST = this.configService.get<string>('REDIS_HOST');
+  private readonly REDIS_PORT = this.configService.get<string>('REDIS_PORT');
+  private readonly REDIS_USERNAME = this.configService.get<string>('REDIS_USERNAME');
+  private readonly REDIS_PASSWORD = this.configService.get<string>('REDIS_PASSWORD');
+  private readonly REDIS_DB = this.configService.get<string>('REDIS_DB');
+
+  public getConnectionOptions(): RedisOptions {
+    return {
+      host: this.REDIS_HOST,
+      port: Number(this.REDIS_PORT),
+      username: this.REDIS_USERNAME,
+      password: this.REDIS_PASSWORD,
+      db: Number(this.REDIS_DB),
+    };
+  }
+}
