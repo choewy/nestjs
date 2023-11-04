ebed2e875e8928e5597f516e7e50bcfef3079fbd
choewy <choewy32@gmail.com>
2023-11-04 17:34:48 +0900

    feature/app module with io adapter

--- nestjs-redis-queue/src/app.module.ts
+++ nestjs-redis-queue/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { BullModule } from '@nestjs/bull';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+
+import { QueueConfigKey } from '@common/enums';
+import { RedisConfig } from '@common/configs';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { TaskModule } from '@module/task';
+import { NotificateModule } from '@module/notificate';
+import { MessageModule } from '@module/message';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    BullModule.forRoot(QueueConfigKey.Queue, new RedisConfig().getBullModuleOptions()),
+    EventEmitterModule.forRoot(),
+    TaskModule,
+    NotificateModule,
+    MessageModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
