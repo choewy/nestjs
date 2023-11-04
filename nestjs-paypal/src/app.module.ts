5cf8425253eb13f6150b9a29686d295020319bf0
choewy <choewy32@gmail.com>
2023-11-04 13:47:46 +0900

    feature/mongoose module connection

--- nestjs-paypal/src/app.module.ts
+++ nestjs-paypal/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { MongoConfig } from '@common/configs';
+import { MongoDBConnectionName } from '@common/enums';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(...new MongoConfig().getModuleOptions(MongoDBConnectionName.Logger)),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
