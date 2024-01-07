5c1d62a95020fe24a8039ee9ee232a408bd266ae
choewy <choewy32@gmail.com>
2024-01-07 16:47:34 +0900

    feature/initialize enemy

--- nestjs-cqrs/src/module/initialize/initialize.module.ts
+++ nestjs-cqrs/src/module/initialize/initialize.module.ts
@@ -1,13 +1,46 @@
 import { DataSource } from 'typeorm';
 
 import { Module, OnApplicationBootstrap } from '@nestjs/common';
-import { Hero, Item } from 'src/entities';
+
+import { Enemy, Hero, Item } from 'src/entities';
+import { EnemyType } from 'src/common';
 
 @Module({})
 export class InitializeModule implements OnApplicationBootstrap {
   constructor(private readonly dataSource: DataSource) {}
 
   async onApplicationBootstrap(): Promise<void> {
+    const enemyRepository = this.dataSource.getRepository(Enemy);
+    await enemyRepository.upsert(
+      enemyRepository.create([
+        {
+          id: 1,
+          type: EnemyType.Dragon,
+          name: 'Red Dragon',
+          exp: 12,
+        },
+        {
+          id: 2,
+          type: EnemyType.Dragon,
+          name: 'Green Dragon',
+          exp: 18,
+        },
+        {
+          id: 3,
+          type: EnemyType.Dragon,
+          name: 'Blue Dragon',
+          exp: 20,
+        },
+        {
+          id: 4,
+          type: EnemyType.Dragon,
+          name: 'Horntail',
+          exp: 52,
+        },
+      ]),
+      { conflictPaths: { id: true } },
+    );
+
     const itemRepository = this.dataSource.getRepository(Item);
     await itemRepository.upsert(
       itemRepository.create(
