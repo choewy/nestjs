af7251a9073f6a1de76f025edfdc32e12df26d4b
choewy <choewy32@gmail.com>
2024-01-07 16:40:58 +0900

    feature/enemy entity

---
+++ nestjs-cqrs/src/entities/enemy.entity.ts
@@ -0,0 +1,29 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+import { AggregateRoot } from '@nestjs/cqrs';
+
+import { EnemyType } from 'src/common';
+
+@Entity()
+export class Enemy extends AggregateRoot {
+  @PrimaryGeneratedColumn({ type: 'int', unsigned: true })
+  readonly id: number;
+
+  @Column({ type: 'varchar', length: 20 })
+  readonly type: EnemyType;
+
+  @Column({ type: 'varchar', length: 50 })
+  name: string;
+
+  @Column({ type: 'smallint', unsigned: true })
+  exp: number;
+
+  @Column({ type: 'int', unsigned: true, default: 0 })
+  deathCount: number;
+
+  killed() {
+    this.deathCount += 1;
+
+    return this;
+  }
+}
